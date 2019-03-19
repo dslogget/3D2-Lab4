573a7da55b22fc16aee314c5c07c21553b28dd4c
Daniel Sloggett <dslogget+git@gmail.com>
2019-03-19 10:01:48 +0000

    Initial Commit

---
+++ App.s
@@ -0,0 +1,16 @@
+	AREA	AsmTemplate, CODE, READONLY
+	IMPORT	main
+
+; (c) Mike Brady, 2011 -- 2019.
+
+	EXPORT	start		
+	IMPORT IO1DIR
+	IMPORT IO1CLR
+	IMPORT IO1SET
+	IMPORT Display
+
+;Main loop
+start
+
+
+	END
